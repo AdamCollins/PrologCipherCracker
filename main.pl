327851c245b86af7b61dc3d97e62113e83196d8f
Adam Collins <adamcollinssecure@gmail.com>
2018-11-18 15:28:32 -0800

    added terms for shifting strings over by given amount. WIP

--- main.pl
+++ main.pl
@@ -4,7 +4,19 @@ word_in_string(W,S):-
     member(W,L).
 
 %Given a char C and a shift amount A, S is character C shiften A times(including looping)
-char_shift(C,A,S):-
-    char_code(C,V),
-    SV is (V-97+A) mod 26 + 97,
-    char_code(S,SV).
+char_shift(C1,A,C2):-
+    C2 is (C1-97+A) mod 26 + 97.    %shifts C1 over by A while looping around if overflow.
+
+
+%--WIP--
+%given a string, S1, S2 is S1 with all chars shifted by A
+string_shift(S1, A, S2):-
+    string_to_list(S1,L1),
+    charlist_shift(L1, A, L2),
+    string_to_list(S2, L2).
+%--WIP--
+%given list of char shift, shift them over by A spaces.
+charlist_shift([],_,_).
+charlist_shift([H1|S1],A,S2):-
+    char_shift(H1,A,SC),    %SC = Shifted character
+    charlist_shift(S1,A,[SC|S2]).
