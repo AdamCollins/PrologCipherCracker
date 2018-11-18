036047cdbbf8422f12b7d4477da79b7fa386eff1
Adam Collins <adamcollinssecure@gmail.com>
2018-11-18 14:13:49 -0800

    first commit

---
+++ main.pl
@@ -0,0 +1,10 @@
+%Given a string S, W is a word inside of S.
+word_in_string(W,S):-
+    split_string(S," ","",L),
+    member(W,L).
+
+%Given a char C and a shift amount A, S is character C shiften A times(including looping)
+char_shift(C,A,S):-
+    char_code(C,V),
+    SV is (V-97+A) mod 26 + 97.
+    char_code(S,SV).
