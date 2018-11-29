85e3b2b776d0fc55655e1e78339e79c94505a9bf
blindacai <linda_cai2014@hotmail.com>
2018-11-28 19:10:39 -0800

    add hill decipher(know the key)

--- hill.pl
+++ hill.pl
@@ -63,18 +63,18 @@ encipher_text([KeyM_head | KeyM_tail], TextM_head, [EcdH | EcdT]) :-
   encipher_text(KeyM_tail, TextM_head, EcdT).
 
 % pre-condition: the length of each element in Key and Text matrix should be the same
-encipher(_, [], []).
-encipher(Key_matrix, [TextM_head | TextM_tail], [EcdH | EcdT]) :-
+cipher(_, [], []).
+cipher(Key_matrix, [TextM_head | TextM_tail], [EcdH | EcdT]) :-
   encipher_text(Key_matrix, TextM_head, Encoded_matrix),
   append([Encoded_matrix, []], EcdH),
-  encipher(Key_matrix, TextM_tail, EcdT).
+  cipher(Key_matrix, TextM_tail, EcdT).
 
 % convert Num list to Char list
 convert_firsttwo([], []).
-convert_firsttwo([EcdH | EcdT], [T0 | [T1 | T2]]) :-
-  X is div(EcdH, 26),
+convert_firsttwo([EcdH | EcdT], [T1 | T2]) :-
+  % X is div(EcdH, 26),
   R is mod(EcdH, 26) + 65,
-  char_code(T0, X),
+  % char_code(T0, X),
   char_code(T1, R),
   convert_firsttwo(EcdT, T2).
 
@@ -85,19 +85,80 @@ convert_to_ciphered_text([EcdH | EcdT], [CtlH | CtlT]) :-
   text_to_string(Text, CtlH),
   convert_to_ciphered_text(EcdT, CtlT).
 
+en_de_cipher(Key_matrix, Target_matrix, Result) :-
+  cipher(Key_matrix, Target_matrix, Encoded_matrix),
+  convert_to_ciphered_text(Encoded_matrix, Ciphered_text_list),
+  atomics_to_string(Ciphered_text_list, Result).
+
 % pre-condition: Key has to be a list of 4 elements which will be viewed as a 2*2 matrix
 hill_encipher(Key, Plain_text, Ciphered_text) :-
   validate_key(Key),
   convert_to_matrix(Key, Key_matrix),
   convert_to_matrix(Plain_text, Text_matrix),
-  encipher(Key_matrix, Text_matrix, Encoded_matrix),
-  convert_to_ciphered_text(Encoded_matrix, Ciphered_text_list),
-  atomics_to_string(Ciphered_text_list, Ciphered_text).
+  en_de_cipher(Key_matrix, Text_matrix, Ciphered_text).
 
 
+% -- not in use
 % hill_encipher("abcd", "abcdefg", E).
 % E = "\000\B\000\D\000\D\000\N\000\F\000\X\001\A\003\M"
 
-% hill_decipher(Ciphered_text, Key, Plain_text).
-% hill_decipher("\000\B\000\D\000\D\000\N\000\F\000\X\001\A\003\M", "abcd", P).
+% hill_decipher("abcd", "\000\B\000\D\000\D\000\N\000\F\000\X\001\A\003\M", P).
 % P = "abcdefg"
+% --
+
+% in use
+% hill_encipher("hill", "shorte", E).
+% E = "APADJT"
+
+% hill_decipher("hill", "APADJT", P).
+% P = "shorte"
+
+choose_Deter(D) :- member(D, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]).
+
+extract_elements(Key_matrix, A, B, C, D) :-
+  nth0(0, Key_matrix, First),
+  nth0(0, First, A),
+  nth0(1, First, B),
+  nth0(1, Key_matrix, Second),
+  nth0(0, Second, C),
+  nth0(1, Second, D).
+
+mod_ele(A, B, C, D, F, S, T, L) :-
+  F is mod(A, 26),
+  S is mod(B, 26),
+  T is mod(C, 26),
+  L is mod(D, 26).
+
+cal_determinant(Key_matrix, Deter) :-
+  extract_elements(Key_matrix, A, B, C, D),
+  M is mod(A * C - B * D, 26),
+  choose_Deter(N),
+  Deter is rdiv(26 * N + 1, M),
+  integer(Deter).
+
+adj_matrix(Key_matrix, Adj_matrix) :-
+  extract_elements(Key_matrix, A, B, C, D),
+  mod_ele(A, -B, -C, D, F, S, T, L),
+  append([[L, S]], [], First),
+  append(First, [[T, F]], Adj_matrix).
+
+get_inverse(Deter, Adj_matrix, Inverse_matrix) :-
+  extract_elements(Adj_matrix, A, B, C, D),
+  A1 is Deter * A,
+  B1 is Deter * B,
+  C1 is Deter * C,
+  D1 is Deter * D,
+  mod_ele(A1, B1, C1, D1, A2, B2, C2, D2),
+  append([[A2, B2]], [], First),
+  append(First, [[C2, D2]], Inverse_matrix).
+
+inverse_matrix(Key_matrix, Inverse_matrix) :-
+  cal_determinant(Key_matrix, Deter),
+  adj_matrix(Key_matrix, Adj_matrix),
+  get_inverse(Deter, Adj_matrix, Inverse_matrix).
+
+hill_decipher(Key, Ciphered_text, Plain_text) :-
+  convert_to_matrix(Key, Key_matrix),
+  convert_to_matrix(Ciphered_text, Ciphered_matrix),
+  inverse_matrix(Key_matrix, Inverse_matrix),
+  en_de_cipher(Inverse_matrix, Ciphered_matrix, Plain_text).
